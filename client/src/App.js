ecab874243e5d76e4ccbfb66826f0ba64f4c86f8
martincavaliers <martincoutts5@gmail.com>
2019-10-12 07:15:35 +0100

    findTotalFlights function working correctly and pushing object to state, being called in componentDidMount

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ export default class App extends Component {
     flightsObject: {},
     flightsArr: [],
     departTimesArr: [],
-    flightsPreNoon: []
+    flightsPreNoon: [],
+    totalflights: {}
   };
 
   // Takes in original object from API
@@ -19,8 +20,37 @@ export default class App extends Component {
       .then(flights => this.setState({ flightsObject: flights }))
       .then(() =>
         this.setState({ flightsArr: this.state.flightsObject.flights.flight })
-      );
+      )
+      .then(() => {
+        this.findTotalFlights(this.state.flightsArr);
+      });
   }
+
+  // Find total number of flights inclduing segments
+  findTotalFlights = arr => {
+    let flightsNoSegments = arr.length;
+    let flightsWithSegments;
+    // Initialise counter var
+    let total = 0;
+    // Maps through given array and checks if segment key exist in given object
+    // If so it gives a count of how many segments there are
+    // Returns to total var outside of function
+    arr.map(flight => {
+      if (flight.hasOwnProperty("segments")) {
+        let segments = flight.segments.segment.length;
+        total = total + segments;
+      }
+    });
+    // Updating var from total
+    flightsWithSegments = total;
+    // Adding object to state to give more flexability at later stage, can either give full journeys or total flights
+    this.setState({
+      totalflights: {
+        totalJourneys: flightsNoSegments,
+        totalflights: flightsWithSegments + flightsNoSegments
+      }
+    });
+  };
 
   flightPreNoon = arr => {
     // Initialising array for pushing map to
@@ -41,6 +71,13 @@ export default class App extends Component {
       flightsPreNoon: departTimesArr.filter(time => time < 120000)
     });
   };
+
+  // Must take in array
+  // Filter out which of the flights destair matches array of Swedish airport codes
+  // Take returned number and work out percentage of total flights
+  // At some point may need to check on segmented flights as well
+
+  flightsToSweden = () => {};
 
   render() {
     return (
