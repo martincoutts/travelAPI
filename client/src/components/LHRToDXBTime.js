b2062c57b661f784b5b7750cf27ef4f71cccd1af
martincavaliers <martincoutts5@gmail.com>
2019-10-14 08:08:16 +0100

    Basic layout for mobile view complete

--- client/src/components/LHRToDXBTime.js
+++ client/src/components/LHRToDXBTime.js
@@ -5,7 +5,7 @@ const LHRToDXBTime = props => {
 
   return (
     <div className="info-card" id="lhr-dxb-time">
-      <h2>Average Flight Time:</h2>
+      <h2>Average Flight Time</h2>
       <h4>LHR - DXB</h4>
 
       {`${props.averageTimeLHRtoDXB.hours} hours ${props.averageTimeLHRtoDXB.mins} minutes`}
